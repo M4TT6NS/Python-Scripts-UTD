6baf62d8c4c6542bed344a56150f12cfc7e75a8f
M4TT6NS <mitvin1947@students.uplifteducation.org>
2026-03-07 16:09:42 -0600

    Add basic calculator functionality
    Implement basic calculator functionality for addition, subtraction, division, and multiplication.

---
+++ MathCalculator.py
@@ -0,0 +1,15 @@
+first_number = int(input("Enter your first number: "))
+print("\n")
+second_number = int(input("Enter your second number: "))
+print("\n")
+symbol = str(input("Enter your symbol: "))
+print("\n")
+
+if symbol == "+":
+    print(first_number + second_number)
+elif symbol == "-":
+    print(first_number + second_number)
+elif symbol == "/":
+    print(first_number + second_number)
+elif symbol == "*":
+    print(first_number * second_number)
